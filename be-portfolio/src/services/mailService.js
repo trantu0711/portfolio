ab798f6ec42b1324b5238537409d381d7d1ca671
trantu0711 <dh52107408@student.stu.edu.vn>
2026-05-08 21:36:51 +0700

    Cập nhật thời gian chờ
    Updated mail service configuration to enforce TLS version and increase timeout settings.

--- be-portfolio/src/services/mailService.js
+++ be-portfolio/src/services/mailService.js
@@ -1,19 +1,23 @@
 import nodemailer from 'nodemailer';
 
 export const sendContactEmail = async (formData) => {
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587, 
-    secure: false, 
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-    tls: {
-      rejectUnauthorized: false // Giúp tránh lỗi kết nối trên một số server
-    }
-  });
-
+ const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // Port 587 bắt buộc false
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+    minVersion: "TLSv1.2" // Ép dùng phiên bản TLS bảo mật chuẩn
+  },
+  connectionTimeout: 20000, // Tăng thời gian chờ lên 20 giây
+  greetingTimeout: 20000,
+  socketTimeout: 20000,
+});
+  
   const mailOptions = {
     from: formData.email,
     to: process.env.EMAIL_USER,
